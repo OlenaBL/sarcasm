8f556a58d89efb73c21930b8e0873ed4c32ecd79
OlenaBL <35238049+OlenaBL@users.noreply.github.com>
2021-05-26 14:36:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -71,12 +71,12 @@ def cleaning(sentence):
 	classifier.fit(X_train, y_train)
 	score = classifier.score(X_train, y_train)
 	
-	if request.method == 'POST':
-		message = request.form['headline']
-		data = [message]
-		vectorizer = classifier.transform(data).toarray()
-		my_prediction = classifier.predict(vectorizer)
-	return render_template('answer.html',prediction = my_prediction)
+if request.method == 'POST':
+	message = request.form['headline']
+	data = [message]
+	vectorizer = vectorizer.transform(data).toarray()
+	my_prediction = classifier.predict(vectorizer)
+return render_template('answer.html',prediction = my_prediction)
 
 if __name__ == '__main__':
 	app.run(debug=True)
